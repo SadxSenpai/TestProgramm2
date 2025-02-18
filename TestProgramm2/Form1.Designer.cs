1d81d8a2905003df22d509c4e3d68543ac67dc06
SadxSenpai <Marvinhauser14@gmail.com>
2025-02-18 13:04:13 +0100

    Projektdateien hinzufügen.

---
+++ TestProgramm2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TestProgramm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            checkBox1 = new CheckBox();
+            checkedListBox1 = new CheckedListBox();
+            checkBox2 = new CheckBox();
+            comboBox1 = new ComboBox();
+            colorDialog1 = new ColorDialog();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            xxToolStripMenuItem = new ToolStripMenuItem();
+            dateTimePicker1 = new DateTimePicker();
+            domainUpDown1 = new DomainUpDown();
+            listView1 = new ListView();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(281, 57);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(42, 107);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(180, 86);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(173, 146);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 94);
+            checkedListBox1.TabIndex = 3;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(45, 210);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(82, 19);
+            checkBox2.TabIndex = 4;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(390, 76);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 5;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { xxToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(85, 26);
+            // 
+            // xxToolStripMenuItem
+            // 
+            xxToolStripMenuItem.Name = "xxToolStripMenuItem";
+            xxToolStripMenuItem.Size = new Size(84, 22);
+            xxToolStripMenuItem.Text = "xx";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(368, 223);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 7;
+            // 
+            // domainUpDown1
+            // 
+            domainUpDown1.Location = new Point(250, 269);
+            domainUpDown1.Name = "domainUpDown1";
+            domainUpDown1.Size = new Size(120, 23);
+            domainUpDown1.TabIndex = 8;
+            domainUpDown1.Text = "domainUpDown1";
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(55, 302);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(121, 97);
+            listView1.TabIndex = 9;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Location = new Point(577, 65);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 100);
+            panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ControlDarkDark;
+            pictureBox1.Location = new Point(590, 247);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Controls.Add(listView1);
+            Controls.Add(domainUpDown1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkedListBox1);
+            Controls.Add(checkBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "xxxxxx";
+            contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private CheckBox checkBox1;
+        private CheckedListBox checkedListBox1;
+        private CheckBox checkBox2;
+        private ComboBox comboBox1;
+        private ColorDialog colorDialog1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem xxToolStripMenuItem;
+        private DateTimePicker dateTimePicker1;
+        private DomainUpDown domainUpDown1;
+        private ListView listView1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+    }
+}
